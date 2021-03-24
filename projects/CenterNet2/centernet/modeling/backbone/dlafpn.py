6fb9d20e9d1d3c3d8a1012c119b78c9374ad42e7
Yonghye Kwon <developer.0hye@gmail.com>
2021-03-24 20:55:17 +0900

    vectorized weight initialization
    vectorized weight initialization

--- projects/CenterNet2/centernet/modeling/backbone/dlafpn.py
+++ projects/CenterNet2/centernet/modeling/backbone/dlafpn.py
@@ -257,10 +257,8 @@ def fill_up_weights(up):
     c = (2 * f - 1 - f % 2) / (2. * f)
     for i in range(w.size(2)):
         for j in range(w.size(3)):
-            w[0, 0, i, j] = \
+            w[:, 0, i, j] = \
                 (1 - math.fabs(i / f - c)) * (1 - math.fabs(j / f - c))
-    for c in range(1, w.size(0)):
-        w[c, 0, :, :] = w[0, 0, :, :]
 
 
 class Conv(nn.Module):
